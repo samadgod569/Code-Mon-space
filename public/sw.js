5c9fcfcbc0f6d211c58a020c385de17f8d8eaef8
samadgod569 <hellohikaisiho124@gmail.com>
2025-12-27 11:55:42 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,37 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("v1").then(cache =>
-      cache.addAll(["./"])
+const CACHE_NAME = "v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./C69P2W_newllo.png",
+  "./C69P2W_newllo.png"
+];
+
+// Install event: cache all files
+self.addEventListener("install", (event) => {
+  console.log("Service Worker: Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event: clean old caches
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activated");
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
     )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res =>
-      res || fetch(e.request)
-    )
+// Fetch event: serve from cache, fallback to network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
