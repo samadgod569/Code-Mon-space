90d94b2892561c7ddd571dc400485f4acf13cb6a
samadgod569 <hellohikaisiho124@gmail.com>
2025-12-27 11:42:39 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("v1").then(cache =>
+      cache.addAll(["./"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res =>
+      res || fetch(e.request)
+    )
+  );
+});
