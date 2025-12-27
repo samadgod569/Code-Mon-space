73c53eeaa5a8fb3018e6990be185a2a5248574ac
samadgod569 <hellohikaisiho124@gmail.com>
2025-12-27 12:07:34 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// Simple log to show JS is working
+console.log("Hello from script.js!");
+
+// Optional: Show alert when page loads
+window.addEventListener("load", () => {
+  console.log("Page loaded!");
+  // alert("Welcome to your PWA!");
+});
