3595cbe94d007db7de581b56f3fd0d7371b85991
samadgod569 <hellohikaisiho124@gmail.com>
2025-12-28 10:46:49 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,25 @@
-const CACHE_NAME = "v2";
-
-// Get app name from query string
-const urlParams = new URL(location).searchParams;
-const appName = urlParams.get('name') || 'defaultApp';
-
+const CACHE_NAME = 'app-v1';
 const urlsToCache = [
-  "./",
-  "./@me",
-  `https://code-mon-space.workers.dev/api/engine?name=${encodeURIComponent(appName)}`,
-  "./C69P2W_newllo.png"
+  './',
+  './test.html',
+  './@me'
 ];
 
-// Install event: cache all files
-self.addEventListener("install", (event) => {
-  console.log("Service Worker: Installed");
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+// Install
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activate event: clean old caches
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker: Activated");
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
+// Activate
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+  ));
 });
 
-// Fetch event: serve from cache, fallback to network
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+// Fetch
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
