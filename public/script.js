45a1d2ee8b5c4388363f7600f6477ce071c4937e
samadgod569 <hellohikaisiho124@gmail.com>
2026-01-28 18:46:58 +0530

    Delete public/script.js

--- public/script.js
+++
@@ -1,8 +0,0 @@
-// Simple log to show JS is working
-console.log("Hello from script.js!");
-
-// Optional: Show alert when page loads
-window.addEventListener("load", () => {
-  console.log("Page loaded!");
-  // alert("Welcome to your PWA!");
-});
